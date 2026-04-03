5c1529e76eff98db3fe4935e8f21eb210312ce5e
Sophie-WZR <164601603+Sophie-WZR@users.noreply.github.com>
2026-04-03 01:41:51 -0700

    Configure GitHub Pages deployment

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/google-trends-facebook-campaign-analytics/",
   plugins: [react()],
 });
